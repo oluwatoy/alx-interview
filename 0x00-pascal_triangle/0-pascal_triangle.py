af10772c8af2fb575f5a2d15c6969da79eeef4f0
Bamidele Emmanuel <bamideleadeiga@gmail.com>
2023-04-20 11:53:14 -0700

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+function pascal(rows) {
+  if (rows === 1) {
+    return [[1]];
+  }
+if (rows === 0) {
+    return [];
+  }
+  const prev = pascal(rows - 1);
+  const last = prev[prev.length - 1];
+  const next = [1];
+  for (let i = 0; i < last.length - 1; i++) {
+    next.push(last[i] + last[i + 1]);
+  }
+  next.push(1);
+  prev.push(next);
+  return prev;
+}
